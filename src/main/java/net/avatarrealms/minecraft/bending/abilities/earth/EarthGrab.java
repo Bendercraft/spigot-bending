9aa8ddfe5e0ad80509fb9f3658bfe2d2414c559b
koudja <koudja@hotmail.fr>
2014-06-27 12:37:19 +0200

    Removed old import

--- src/main/java/net/avatarrealms/minecraft/bending/abilities/earth/EarthGrab.java
+++ src/main/java/net/avatarrealms/minecraft/bending/abilities/earth/EarthGrab.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
-
 import net.avatarrealms.minecraft.bending.controller.ConfigManager;
 import net.avatarrealms.minecraft.bending.model.Abilities;
 import net.avatarrealms.minecraft.bending.model.BendingPlayer;
