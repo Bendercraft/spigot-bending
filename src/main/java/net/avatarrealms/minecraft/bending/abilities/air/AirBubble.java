1a16c82056febaf7d4a2b86e17f08bebdba120a7
koudja <koudja@hotmail.fr>
2014-08-10 21:50:21 +0200

    No longer process AirBubble/WaterBubble if player location has not changed

--- src/main/java/net/avatarrealms/minecraft/bending/abilities/air/AirBubble.java
+++ src/main/java/net/avatarrealms/minecraft/bending/abilities/air/AirBubble.java
@@ -31,6 +31,7 @@ public class AirBubble implements IAbility {
 
 	private Player player;
 	private double radius;
+	private Location lastLocation;
 
 	private Map<Block, BlockState> waterorigins;
 
@@ -46,60 +47,62 @@ public class AirBubble implements IAbility {
 		
 		this.parent = parent;
 		this.player = player;
+		this.lastLocation = player.getLocation();
 		waterorigins = new HashMap<Block, BlockState>();
 		instances.put(player.getEntityId(), this);
 	}
 
 	private void pushWater() {
-		if (EntityTools.isBender(player, BendingType.Water)) {
-			radius = defaultWaterRadius;
-			if (Tools.isNight(player.getWorld())) {
-				radius = PluginTools.waterbendingNightAugment(defaultWaterRadius,
-						player.getWorld());
-			}
-		}
-		else {
-			radius = defaultAirRadius;
-		}
-
-		if (defaultAirRadius > radius
-				&& EntityTools.isBender(player, BendingType.Air)) {
-			radius = defaultAirRadius;
-			// In case he has both element
-		}
-				
 		Location location = player.getLocation();
-
-		List<Block> toRemove = new LinkedList<Block>();
-		for (Entry<Block, BlockState> entry : waterorigins.entrySet()) {
-			if (entry.getKey().getWorld() != location.getWorld()) {
-				if (entry.getKey().getType() == Material.AIR || BlockTools.isWater(entry.getKey()))
-					entry.getValue().update(true);
-				toRemove.add(entry.getKey());
-			} else if (entry.getKey().getLocation().distance(location) > radius) {
-				if (entry.getKey().getType() == Material.AIR || BlockTools.isWater(entry.getKey()))
-					entry.getValue().update(true);
-				toRemove.add(entry.getKey());
-			}
-		}
 		
-		for(Block block : toRemove) {
-			waterorigins.remove(block);
-		}
-
-		for (Block block : BlockTools.getBlocksAroundPoint(location, radius)) {
-			if (waterorigins.containsKey(block))
-				continue;
-			if (Tools.isRegionProtectedFromBuild(player, Abilities.AirBubble,
-					block.getLocation()))
-				continue;
-			if (block.getType() == Material.STATIONARY_WATER
-					|| block.getType() == Material.WATER) {
-				
-				if (WaterManipulation.canBubbleWater(block)) {
+		//Do not bother entering this loop if player location has not been modified
+		if(!this.lastLocation.equals(location)) {
+			if (EntityTools.isBender(player, BendingType.Water)) {
+				radius = defaultWaterRadius;
+				if (Tools.isNight(player.getWorld())) {
+					radius = PluginTools.waterbendingNightAugment(defaultWaterRadius,
+							player.getWorld());
+				}
+			}
+			else {
+				radius = defaultAirRadius;
+			}
+	
+			if (defaultAirRadius > radius
+					&& EntityTools.isBender(player, BendingType.Air)) {
+				radius = defaultAirRadius;
+				// In case he has both element
+			}
 					
-					waterorigins.put(block, block.getState());
-					block.setType(Material.AIR);
+			
+	
+			List<Block> toRemove = new LinkedList<Block>();
+			for (Entry<Block, BlockState> entry : waterorigins.entrySet()) {
+				if (entry.getKey().getWorld() != location.getWorld()) {
+					toRemove.add(entry.getKey());
+				} else if (entry.getKey().getLocation().distance(location) > radius) {
+					toRemove.add(entry.getKey());
+				}
+			}
+			
+			for(Block block : toRemove) {
+				if (block.getType() == Material.AIR || BlockTools.isWater(block))
+					waterorigins.get(block).update(true);
+				waterorigins.remove(block);
+			}
+	
+			for (Block block : BlockTools.getBlocksAroundPoint(location, radius)) {
+				if (waterorigins.containsKey(block))
+					continue;
+				if (Tools.isRegionProtectedFromBuild(player, Abilities.AirBubble,
+						block.getLocation()))
+					continue;
+				if (block.getType() == Material.STATIONARY_WATER
+						|| block.getType() == Material.WATER) {
+					if (WaterManipulation.canBubbleWater(block)) {
+						waterorigins.put(block, block.getState());
+						block.setType(Material.AIR);
+					}
 				}
 			}
 		}
