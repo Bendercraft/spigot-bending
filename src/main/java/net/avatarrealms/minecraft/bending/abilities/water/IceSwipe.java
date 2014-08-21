a79dd9849f6fd0f61da9a139da394db04122f871
Kiratsu <deboisjerome@gmail.com>
2014-08-21 21:30:49 +0200

    Advance in the IceSwipe(1)

--- src/main/java/net/avatarrealms/minecraft/bending/abilities/water/IceSwipe.java
+++ src/main/java/net/avatarrealms/minecraft/bending/abilities/water/IceSwipe.java
@@ -7,12 +7,15 @@ import java.util.Map;
 
 import org.bukkit.Location;
 import org.bukkit.block.Block;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 
 import net.avatarrealms.minecraft.bending.controller.ConfigManager;
 import net.avatarrealms.minecraft.bending.model.Abilities;
 import net.avatarrealms.minecraft.bending.model.BendingPlayer;
 import net.avatarrealms.minecraft.bending.model.IAbility;
+import net.avatarrealms.minecraft.bending.utils.EntityTools;
+import net.avatarrealms.minecraft.bending.utils.Tools;
 
 public class IceSwipe implements IAbility{
 	
@@ -26,11 +29,14 @@ public class IceSwipe implements IAbility{
 	private IAbility parent;
 	private Player player;
 	private BendingPlayer bPlayer;
-	private boolean started;
+	//private long time;
+	
 	private Location origin; // Could be useless
 	private Location targettedLocation;
 	
-	//TODO : Not to forget to check for the protected region
+	private boolean started;
+	private boolean ready;
+
 	//TODO : As Kya against Zaheer
 	
 	public IceSwipe(Player player, Block sourceblock, IAbility parent) {
@@ -45,12 +51,13 @@ public class IceSwipe implements IAbility{
 		blocks = new LinkedList<Block>();
 		started = false;
 		this.parent = parent;
+		//time = System.currentTimeMillis();
 		origin = sourceblock.getLocation();
 		
 	}
 	
 	public static void prepare(Player player) {
-		
+		//TODO : Not to forget to check for the protected region
 		
 	}
 	
@@ -78,12 +85,57 @@ public class IceSwipe implements IAbility{
 			if (! player.isSneaking()) {
 				return false;
 			}
-		}
-		
+			
+			if (Tools.isRegionProtectedFromBuild(player, Abilities.IceSwipe, player.getLocation())) {
+				return false;
+			}
+			
+			if (!ready) {
+				
+			}
+			
+			if (ready) {
+				manageBlocks();
+			}
+			
+		}	
 		return true;
 	}
 	
+	public void manageBlocks() {
+		List<Block> blocksToRemove = new LinkedList<Block>();
+		for (Block block : blocks) {
+			if (block.getLocation().distance(player.getLocation()) > range) {
+				blocksToRemove.add(block);
+				continue;
+			}
+			
+			if (Tools.isRegionProtectedFromBuild(player, Abilities.IceSwipe, block.getLocation())) {
+				blocksToRemove.add(block);
+				continue;
+			}
+			
+			// I feel it will be very laggy
+			for (LivingEntity entity : EntityTools.getLivingEntitiesAroundPoint(block.getLocation(), 1)) {
+				if (entity.getUniqueId() == player.getUniqueId()) {
+					continue;	
+				}
+				blockHit(block, entity);
+				blocksToRemove.add(block);
+			}
+		}
+		
+		for (Block block : blocksToRemove) {
+			blocks.remove(block);
+		}
+	}
+	
 	public void moveWaterAround() {
+		
+	}
+	
+	public void blockHit(Block block, LivingEntity entity) {
+		// Don't know if 'block' is going to be useful, may disappear
 		
 	}
 	
