7dcdc666dad2f20ae68f645b6ad14b4f76d2fb45
Kiratsu <deboisjerome@gmail.com>
2014-08-21 20:59:29 +0200

    Very small advance in the IceSwipe

--- src/main/java/net/avatarrealms/minecraft/bending/abilities/water/IceSwipe.java
+++ src/main/java/net/avatarrealms/minecraft/bending/abilities/water/IceSwipe.java
@@ -2,28 +2,50 @@ package net.avatarrealms.minecraft.bending.abilities.water;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 import net.avatarrealms.minecraft.bending.controller.ConfigManager;
+import net.avatarrealms.minecraft.bending.model.Abilities;
+import net.avatarrealms.minecraft.bending.model.BendingPlayer;
 import net.avatarrealms.minecraft.bending.model.IAbility;
 
 public class IceSwipe implements IAbility{
 	
+	private static Map<Player, IceSwipe> instances = new HashMap<Player, IceSwipe>();
 	
 	private static int range = ConfigManager.iceSwipeRange;
 	private static int damage = ConfigManager.iceSwipeDamage;
-	private static Map<Player, IceSwipe> instances = new HashMap<Player, IceSwipe>();
-	private LinkedList<Block> blocks;
+	
+	private List<Block> blocks;
 	
 	private IAbility parent;
+	private Player player;
+	private BendingPlayer bPlayer;
+	private boolean started;
+	private Location origin; // Could be useless
+	private Location targettedLocation;
 	
 	//TODO : Not to forget to check for the protected region
 	//TODO : As Kya against Zaheer
 	
-	public IceSwipe(Player player, Block sourceblock) {
+	public IceSwipe(Player player, Block sourceblock, IAbility parent) {
+		
+		bPlayer = BendingPlayer.getBendingPlayer(player);
+		
+		if (bPlayer.isOnCooldown(Abilities.IceSwipe)) {
+			return;
+		}
+		
+		this.player = player;
+		blocks = new LinkedList<Block>();
+		started = false;
+		this.parent = parent;
+		origin = sourceblock.getLocation();
 		
 	}
 	
@@ -33,12 +55,44 @@ public class IceSwipe implements IAbility{
 	}
 	
 	public static void progressAll() {
+		List<Player> toRemove = new LinkedList<Player>();
+		for (Player pl : instances.keySet()) {
+			boolean keep = instances.get(pl).progress();
+			if (!keep) {
+				toRemove.add(pl);
+			}
+		}
 		
+		for (Player pl : toRemove) {
+			instances.remove(pl);
+		}
 	}
 	
 	public boolean progress() {
 		
+		if (!player.isOnline() || player.isDead()) {
+			return false;
+		}
+		
+		if (started) {
+			if (! player.isSneaking()) {
+				return false;
+			}
+		}
+		
 		return true;
+	}
+	
+	public void moveWaterAround() {
+		
+	}
+	
+	public void retargetBlocks() {
+		
+	}
+	
+	public void launchBlock() {
+		
 	}
 
 	
