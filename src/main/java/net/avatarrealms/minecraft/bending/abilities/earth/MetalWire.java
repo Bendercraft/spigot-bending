bd794e36d498d59bb9dce75ff68e9e297a16da7b
Kiratsu <deboisjerome@gmail.com>
2014-08-28 01:34:34 +0200

    Better version of the metal wire : More precise

--- src/main/java/net/avatarrealms/minecraft/bending/abilities/earth/MetalWire.java
+++ src/main/java/net/avatarrealms/minecraft/bending/abilities/earth/MetalWire.java
@@ -15,24 +15,33 @@ public class MetalWire {
 	private static List<Player> players = new LinkedList<Player>();
 	
 	public static void pull(Player player, Fish hook) {
-		Bukkit.getLogger().info("Entered the pull method");
-		if (players.contains(player)) {
-			Bukkit.getLogger().info("Contains");
+		if (players.contains(player)) {	
+			Bukkit.getLogger().info("Bounce");
 			Location loc = hook.getLocation().add(0,-1,0);
 			if (!BlockTools.isFluid(loc.getBlock())) {
-				Bukkit.getLogger().info("Fluid");
-				Location targetLoc = loc.clone().add(0,1,0);
+				Bukkit.getLogger().info("Not Fluid");
+				Location targetLoc = loc.clone().add(0, 1.5, 0);
 				Location playerLoc = player.getLocation();
 				
-				Vector dir = targetLoc.subtract(playerLoc).toVector();
+				Vector dir = getVectorForPoints(playerLoc, targetLoc);
 				player.setVelocity(dir);	
 			}
 			players.remove(player);
 		} 
 		else {
-			Bukkit.getLogger().info("Doesn't contain");
+			Bukkit.getLogger().info("Doesn't bounce");
 			//if the list doesn't contain the player, it means he just launched the hook
 			players.add(player);
 		}
+	}
+	
+	public static Vector getVectorForPoints (Location l1, Location l2) {
+		double g = -0.08;
+		double d = l2.distance(l1);
+		double t = d;
+		double vX = (1.0 + 0.07*t)*(l2.getX() - l1.getX())/t;
+		double vY = (1.0 + 0.03*t)*(l2.getY() - l1.getY())/t - 0.5*g*t;
+		double vZ = (1.0 + 0.07*t)*(l2.getZ() - l1.getZ())/t;
+		return new Vector(vX, vY, vZ);
 	}
 }
