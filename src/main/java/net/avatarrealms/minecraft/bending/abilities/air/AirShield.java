86b28d4e3682ae3d51ad4728dfdcfb3d1effc7bf
Kiratsu <deboisjerome@gmail.com>
2015-01-01 20:39:26 +0100

    AirShield now extinguish player in it

--- src/main/java/net/avatarrealms/minecraft/bending/abilities/air/AirShield.java
+++ src/main/java/net/avatarrealms/minecraft/bending/abilities/air/AirShield.java
@@ -25,12 +25,13 @@ import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
+
 public class AirShield implements IAbility {
 
 	private static Map<Integer, AirShield> instances = new HashMap<Integer, AirShield>();
 
 	private static double maxradius = ConfigManager.airShieldRadius;
-	private static int numberOfStreams = (int) (.75 * (double) maxradius);
+	private static int numberOfStreams = (int)(.75 * (double)maxradius);
 
 	private double radius = 2;
 
@@ -40,47 +41,50 @@ public class AirShield implements IAbility {
 	private Map<Integer, Integer> angles = new HashMap<Integer, Integer>();
 	private IAbility parent;
 
-	public AirShield(Player player, IAbility parent) {
+	public AirShield (Player player, IAbility parent) {
 		this.parent = parent;
-		if (AvatarState.isAvatarState(player)
-				&& instances.containsKey(player.getEntityId())) {
+		if (AvatarState.isAvatarState(player) && instances.containsKey(player.getEntityId())) {
 			instances.remove(player.getEntityId());
 			return;
 		}
 		this.player = player;
 		int angle = 0;
-		int di = (int) (maxradius * 2 / numberOfStreams);
-		for (int i = -(int) maxradius + di; i < (int) maxradius; i += di) {
-			angles.put(i, angle);
+		int di = (int)((maxradius * 2) / numberOfStreams);
+		for (int i = -(int)maxradius + di ; i < (int)maxradius ; i += di) {
+			this.angles.put(i, angle);
 			angle += 90;
-			if (angle == 360)
+			if (angle == 360) {
 				angle = 0;
+			}
 		}
 
 		instances.put(player.getEntityId(), this);
 		BendingPlayer.getBendingPlayer(this.player).cooldown(Abilities.AirShield);
 	}
 
-	private void rotateShield() {
-		Location origin = player.getLocation();
-		
-		FireBlast.removeFireBlastsAroundPoint(origin, radius);
+	private void rotateShield () {
+		Location origin = this.player.getLocation();
 
-		for (Entity entity : EntityTools.getEntitiesAroundPoint(origin, radius)) {
-			if(ProtectionManager.isEntityProtectedByCitizens(entity)) {
+		FireBlast.removeFireBlastsAroundPoint(origin, this.radius);
+
+		for (Entity entity : EntityTools.getEntitiesAroundPoint(origin, this.radius)) {
+			if (ProtectionManager.isEntityProtectedByCitizens(entity)) {
 				continue;
 			}
-			
-			if ((entity instanceof ExperienceOrb) || (entity instanceof FallingBlock)
-					|| (entity instanceof ItemFrame) || (entity instanceof Item)) {
+
+			if ((entity instanceof ExperienceOrb) || (entity instanceof FallingBlock) || (entity instanceof ItemFrame)
+					|| (entity instanceof Item)) {
 				continue;
 			}
-			
-			if (ProtectionManager.isRegionProtectedFromBending(player, Abilities.AirShield,
-					entity.getLocation())){
+
+			if (ProtectionManager.isRegionProtectedFromBending(this.player, Abilities.AirShield, entity.getLocation())) {
 				continue;
 			}
-				
+
+			if (entity instanceof Player) {
+				entity.setFireTicks(0);
+			}
+
 			if (origin.distance(entity.getLocation()) > 2) {
 				double x, z, vx, vz, mag;
 				double angle = 50;
@@ -89,111 +93,105 @@ public class AirShield implements IAbility {
 				x = entity.getLocation().getX() - origin.getX();
 				z = entity.getLocation().getZ() - origin.getZ();
 
-				mag = Math.sqrt(x * x + z * z);
+				mag = Math.sqrt((x * x) + (z * z));
 
-				vx = (x * Math.cos(angle) - z * Math.sin(angle)) / mag;
-				vz = (x * Math.sin(angle) + z * Math.cos(angle)) / mag;
+				vx = ((x * Math.cos(angle)) - (z * Math.sin(angle))) / mag;
+				vz = ((x * Math.sin(angle)) + (z * Math.cos(angle))) / mag;
 
 				Vector velocity = entity.getVelocity();
-				if (AvatarState.isAvatarState(player)) {
+				if (AvatarState.isAvatarState(this.player)) {
 					velocity.setX(AvatarState.getValue(vx));
 					velocity.setZ(AvatarState.getValue(vz));
-				} else {
+				}
+				else {
 					velocity.setX(vx);
 					velocity.setZ(vz);
 				}
 
-				velocity.multiply(radius / maxradius);
+				velocity.multiply(this.radius / maxradius);
 				entity.setVelocity(velocity);
 				entity.setFallDistance(0);
 			}
 		}
-		
-		Set<Integer> keys = angles.keySet();
+
+		Set<Integer> keys = this.angles.keySet();
 		for (int i : keys) {
 			double x, y, z;
-			double angle = (double) angles.get(i);
+			double angle = (double)this.angles.get(i);
 			angle = Math.toRadians(angle);
 
-			double factor = radius / maxradius;
+			double factor = this.radius / maxradius;
 
-			y = origin.getY() + factor * (double) i;
+			y = origin.getY() + (factor * (double)i);
 
 			// double theta = Math.asin(y/radius);
-			double f = Math.sqrt(1 - factor * factor * ((double) i / radius)
-					* ((double) i / radius));
+			double f = Math.sqrt(1 - (factor * factor * ((double)i / this.radius) * ((double)i / this.radius)));
 
-			x = origin.getX() + radius * Math.cos(angle) * f;
-			z = origin.getZ() + radius * Math.sin(angle) * f;
+			x = origin.getX() + (this.radius * Math.cos(angle) * f);
+			z = origin.getZ() + (this.radius * Math.sin(angle) * f);
 
 			Location effect = new Location(origin.getWorld(), x, y, z);
-			if (!ProtectionManager.isRegionProtectedFromBending(player, Abilities.AirShield,
-					effect)) {
-				origin.getWorld().playEffect(effect, Effect.SMOKE, 4,
-						(int) AirBlast.defaultrange);
+			if (!ProtectionManager.isRegionProtectedFromBending(this.player, Abilities.AirShield, effect)) {
+				origin.getWorld().playEffect(effect, Effect.SMOKE, 4, (int)AirBlast.defaultrange);
 			}
-				
 
-			angles.put(i, angles.get(i) + (int) (10 * speedfactor));
+			this.angles.put(i, this.angles.get(i) + (int)(10 * this.speedfactor));
 		}
 
-		if (radius < maxradius) {
-			radius += .3;
+		if (this.radius < maxradius) {
+			this.radius += .3;
 		}
 
-		if (radius > maxradius) {
-			radius = maxradius;
+		if (this.radius > maxradius) {
+			this.radius = maxradius;
 		}
-			
 
 	}
 
-	public boolean progress() {
-		if (player.isDead() || !player.isOnline()) {
+	public boolean progress () {
+		if (this.player.isDead() || !this.player.isOnline()) {
 			return false;
 		}
-		if (ProtectionManager.isRegionProtectedFromBending(player, Abilities.AirShield,
-				player.getLocation())) {
+		if (ProtectionManager.isRegionProtectedFromBending(this.player, Abilities.AirShield, this.player.getLocation())) {
 			return false;
 		}
-		speedfactor = 1;
-		if (!EntityTools.canBend(player, Abilities.AirShield)
-				|| player.getEyeLocation().getBlock().isLiquid()) {
+		this.speedfactor = 1;
+		if (!EntityTools.canBend(this.player, Abilities.AirShield) || this.player.getEyeLocation().getBlock().isLiquid()) {
 			return false;
 		}
-		if (((EntityTools.getBendingAbility(player) != Abilities.AirShield) || (!player
-				.isSneaking())) && !AvatarState.isAvatarState(player)) {
+		if (((EntityTools.getBendingAbility(this.player) != Abilities.AirShield) || (!this.player.isSneaking()))
+				&& !AvatarState.isAvatarState(this.player)) {
 			return false;
 		}
 		rotateShield();
 		return true;
 	}
-	
-	public static void progressAll() {
+
+	public static void progressAll () {
 		List<AirShield> toRemove = new LinkedList<AirShield>();
-		for(AirShield shield : instances.values()) {
+		for (AirShield shield : instances.values()) {
 			boolean keep = shield.progress();
-			if(!keep) {
+			if (!keep) {
 				toRemove.add(shield);
 			}
 		}
-		
-		for(AirShield shield : toRemove) {
+
+		for (AirShield shield : toRemove) {
 			shield.remove();
 		}
 	}
-	
-	private void remove() {
+
+	private void remove () {
 		instances.remove(this.player.getEntityId());
 	}
 
-	public static void removeAll() {
+	public static void removeAll () {
 		instances.clear();
 	}
 
 	@Override
-	public IAbility getParent() {
-		return parent;
+	public IAbility getParent () {
+		return this.parent;
 	}
 
 }
