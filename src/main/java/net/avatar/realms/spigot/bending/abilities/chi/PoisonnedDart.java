bdbdcfd7900068e217ab8a312c0e93edf5221424
Noko <nokorbis@gmail.com>
2015-07-28 00:24:22 +0200

    Correct many bugs about PoisonnedDart

--- src/main/java/net/avatar/realms/spigot/bending/abilities/chi/PoisonnedDart.java
+++ src/main/java/net/avatar/realms/spigot/bending/abilities/chi/PoisonnedDart.java
@@ -1,5 +1,8 @@
 package net.avatar.realms.spigot.bending.abilities.chi;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -7,6 +10,9 @@ import org.bukkit.Sound;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.material.Skull;
+import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
@@ -30,7 +36,7 @@ public class PoisonnedDart extends Ability{
 	private Location origin;
 	private Location location;
 	private Vector direction;
-	private PotionEffect effect;
+	private List<PotionEffect> effects;
 	private boolean started;
 	
 	public PoisonnedDart(Player player) {
@@ -52,21 +58,58 @@ public class PoisonnedDart extends Ability{
 		started = true;
 		
 		ItemStack is = player.getItemInHand();
-		if (is.getType() == Material.MILK_BUCKET) {
-			effect = null;
-			is.setType(Material.BUCKET);
-			is.setAmount(1);
-		} else if (is.getType() == Material.POTION) {
-			effect = EntityTools.fromItemStack(is);
-			is.setType(Material.GLASS_BOTTLE);
-			is.setAmount(1);
-		} else if (is.getType() == Material.EYE_OF_ENDER) {
-			effect = new PotionEffect(PotionEffectType.BLINDNESS,20*10,1);
-		} else if (is.getType() == Material.MUSHROOM_SOUP) {
-			effect = new PotionEffect(PotionEffectType.CONFUSION,20*12,1);
-		} else {
-			effect = new PotionEffect(PotionEffectType.POISON, 20*1, 0);
+		effects = new LinkedList<PotionEffect>();
+		switch (is.getType()) {
+			case MILK_BUCKET:
+				effects = null;
+				is.setType(Material.BUCKET);
+				is.setAmount(1);
+				break;
+			case POTION:
+				Potion potion = Potion.fromItemStack(is);
+				effects.addAll(potion.getEffects());
+				is.setType(Material.GLASS_BOTTLE);
+				is.setAmount(1);
+				break;
+			case EYE_OF_ENDER:
+				effects.add(new PotionEffect(PotionEffectType.BLINDNESS,20*10,1));
+				if (is.getAmount() == 1) {
+					is.setType(Material.AIR);
+				}
+				else {
+					is.setAmount(is.getAmount() - 1);
+				}
+				break;
+			case MUSHROOM_SOUP:
+				effects.add(new PotionEffect(PotionEffectType.CONFUSION,20*12,1));
+				is.setType(Material.BOWL);
+				is.setAmount(1);
+				break;
+			case SKULL_ITEM:
+				// Need to detect if Wither Skull
+				break;
+			default : 
+				effects.add(new PotionEffect(PotionEffectType.POISON, 20*1, 0));
+				break;
 		}
+		
+		
+//		if (is.getType() == Material.MILK_BUCKET) {
+//			effect = null;
+//			is.setType(Material.BUCKET);
+//			is.setAmount(1);
+//		} else if (is.getType() == Material.POTION) {
+//			effe
+//			effect = EntityTools.fromItemStack(is);
+//			is.setType(Material.GLASS_BOTTLE);
+//			is.setAmount(1);
+//		} else if (is.getType() == Material.EYE_OF_ENDER) {
+//			effect = new PotionEffect(PotionEffectType.BLINDNESS,20*10,1);
+//		} else if (is.getType() == Material.MUSHROOM_SOUP) {
+//			effect = new PotionEffect(PotionEffectType.CONFUSION,20*12,1);
+//		} else {
+//			effect = new PotionEffect(PotionEffectType.POISON, 20*1, 0);
+//		}
 
 		origin = player.getEyeLocation();
 		location = origin.clone();
@@ -107,33 +150,50 @@ public class PoisonnedDart extends Ability{
 			return false;
 		}		
 		int cptEnt = 0;
-		for (LivingEntity entity : EntityTools.getLivingEntitiesAroundPoint(location, 1.9)) {
+		boolean health = areHealthEffects();
+		for (LivingEntity entity : EntityTools.getLivingEntitiesAroundPoint(location, 2.1)) {
 			if (entity.getEntityId() == player.getEntityId()) {
 				continue;
 			}
-			boolean health = false;
-			if (effect != null) {
-				Bukkit.getLogger().info(effect.getType().getName());
-				if (effect.getType().equals(PotionEffectType.HEAL)
-						|| effect.getType().equals(PotionEffectType.HEALTH_BOOST)
-						|| effect.getType().equals(PotionEffectType.REGENERATION)) {
-					health = true;
+			
+			if (effects == null) {
+				for (PotionEffect effect : entity.getActivePotionEffects()) {
+					entity.removePotionEffect(effect.getType());
 				}
-				entity.addPotionEffect(effect);	
-			}
-			else {
 				entity.getActivePotionEffects().clear();
 			}
-			if (!health) {
+			else {
+				for (PotionEffect effect : effects) {
+					Bukkit.getLogger().info(effect.getType().getName());
+					entity.addPotionEffect(effect);	
+				}
+			}
+			if (!health && effects != null) {
 				EntityTools.damageEntity(player, entity, damage);
 			}
 			cptEnt++;
+			break;
 		}
 
 		if (cptEnt > 0) {
 			return false;
 		}
 		return true;
+	}
+	
+	private boolean areHealthEffects() {
+		if (effects == null) {
+			return false;
+		}
+		for (PotionEffect effect : effects) {
+			if (effect.getType().equals(PotionEffectType.HEAL)
+					|| effect.getType().equals(PotionEffectType.HEALTH_BOOST)
+					|| effect.getType().equals(PotionEffectType.REGENERATION)) {
+				return true;
+			}
+		}		
+		
+		return false;
 	}
 	private void advanceLocation() {
 		VISUAL.display(0, 0, 0, 1, 1, location, 20);
