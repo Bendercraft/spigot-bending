2b60a00126b15db65bbbd4001d656638b2ae3c06
koudja <koudja@hotmail.fr>
2015-08-31 11:10:43 +0100

    Adding BendingPath, not used yet however

---
+++ src/main/java/net/avatar/realms/spigot/bending/abilities/BendingPathType.java
@@ -0,0 +1,46 @@
+package net.avatar.realms.spigot.bending.abilities;
+
+public enum BendingPathType {
+	None 		(BendingType.None),
+	
+	Spiritual	(BendingType.Air),
+	Mobile		(BendingType.Air),
+	Renegade	(BendingType.Air),
+	
+	Balanced	(BendingType.Water),
+	Marksman	(BendingType.Water),
+	Flowless	(BendingType.Water),
+	
+	Control		(BendingType.Fire),
+	Nurture		(BendingType.Fire),
+	Lifeless	(BendingType.Fire),
+	
+	Patient		(BendingType.Earth),
+	Tough		(BendingType.Earth),
+	Reckless	(BendingType.Earth),
+	
+	Equality	(BendingType.ChiBlocker),
+	Seeker		(BendingType.ChiBlocker),
+	Restless	(BendingType.ChiBlocker);
+	
+	
+
+	private BendingType element;
+	
+	BendingPathType(BendingType element) {
+		this.element = element;
+	}
+	
+	public static BendingPathType getType(String string) {
+		for (BendingPathType type : BendingPathType.values()) {
+			if (type.toString().equalsIgnoreCase(string))
+				return type;
+		}
+		return null;
+	}
+	
+	public BendingType getElement() {
+		return element;
+	}
+
+}
