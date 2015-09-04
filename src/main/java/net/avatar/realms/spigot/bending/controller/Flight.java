00ac6722625f46d0060043cecc56a3e4d62a6ada
Noko <nokorbis@gmail.com>
2015-09-04 22:43:29 +0200

    Remove Flight from Avatar State

--- src/main/java/net/avatar/realms/spigot/bending/controller/Flight.java
+++ src/main/java/net/avatar/realms/spigot/bending/controller/Flight.java
@@ -21,58 +21,58 @@ import net.avatar.realms.spigot.bending.abilities.water.WaterSpout;
 @Deprecated
 //Use Flying Player instead
 public class Flight {
-
+	
 	private static Map<UUID, Flight> instances = new HashMap<UUID, Flight>();
-
+	
 	private static long duration = 5000;
-
+	
 	private Player source = null;
 	private Player player = null;
 	private boolean couldFly = false;
 	private boolean wasFlying = false;
 	private long time;
-
-	public Flight(Player player) {
+	
+	public Flight (Player player) {
 		this(player, null);
 	}
-
-	public Flight(Player player, Player source) {
+	
+	public Flight (Player player, Player source) {
 		if (instances.containsKey(player)) {
 			Flight flight = instances.get(player);
 			flight.refresh(source);
 			return;
 		}
-
+		
 		this.couldFly = player.getAllowFlight();
 		this.wasFlying = player.isFlying();
 		this.player = player;
 		this.time = System.currentTimeMillis();
 		instances.put(player.getUniqueId(), this);
 	}
-
-	private void revert() {
+	
+	private void revert () {
 		this.player.setAllowFlight(this.couldFly);
 		this.player.setFlying(this.wasFlying);
 	}
-
-	private void remove() {
+	
+	private void remove () {
 		revert();
 		instances.remove(this.player);
 	}
-
-	private void refresh(Player source) {
+	
+	private void refresh (Player source) {
 		this.time = System.currentTimeMillis();
 		instances.put(this.player.getUniqueId(), this);
 	}
-
-	public static void handle() {
+	
+	public static void handle () {
 		List<Player> players = new LinkedList<Player>();
 		List<Player> newflyingplayers = new LinkedList<Player>();
 		List<Player> avatarstateplayers = new LinkedList<Player>();
 		List<Player> airscooterplayers = new LinkedList<Player>();
 		List<Player> waterspoutplayers = new LinkedList<Player>();
 		List<Player> airspoutplayers = AirSpout.getPlayers();
-
+		
 		players.addAll(Tornado.getPlayers());
 		//players.addAll(Speed.getPlayers());
 		players.addAll(FireJet.getPlayers());
@@ -80,13 +80,11 @@ public class Flight {
 		avatarstateplayers = AvatarState.getPlayers();
 		airscooterplayers = AirScooter.getPlayers();
 		waterspoutplayers = WaterSpout.getPlayers();
-
+		
 		List<Flight> toRemove = new LinkedList<Flight>();
 		for (UUID player : instances.keySet()) {
 			Flight flight = instances.get(player);
-			if (avatarstateplayers.contains(player)
-					|| airscooterplayers.contains(player)
-					|| waterspoutplayers.contains(player)
+			if (avatarstateplayers.contains(player) || airscooterplayers.contains(player) || waterspoutplayers.contains(player)
 					|| airspoutplayers.contains(player)) {
 				continue;
 			}
@@ -95,7 +93,7 @@ public class Flight {
 				flight.player.setFlying(false);
 				continue;
 			}
-
+			
 			if (players.contains(player)) {
 				flight.refresh(null);
 				flight.player.setAllowFlight(true);
@@ -105,23 +103,24 @@ public class Flight {
 				newflyingplayers.add(flight.player);
 				continue;
 			}
-
+			
 			if (flight.source == null) {
 				flight.revert();
 				toRemove.add(flight);
-			} else {
+			}
+			else {
 				if (System.currentTimeMillis() > (flight.time + duration)) {
 					toRemove.add(flight);
 				}
 			}
 		}
 		
-		for(Flight flight : toRemove) {
+		for (Flight flight : toRemove) {
 			flight.remove();
 		}
 	}
-
-	public static void removeAll() {
+	
+	public static void removeAll () {
 		for (Flight flight : instances.values()) {
 			if (flight.source != null) {
 				flight.revert();
@@ -129,21 +128,21 @@ public class Flight {
 		}
 		instances.clear();
 	}
-
+	
 	public static boolean revert (Player player) {
 		if (player == null) {
 			return false;
 		}
-
+		
 		Flight flight = instances.get(player.getUniqueId());
-
+		
 		if (flight == null) {
 			return false;
 		}
 		
 		flight.remove();
-
+		
 		return true;
 	}
-
+	
 }
