715d90e822ede99ecca8c8346f92709c4cb360d7
koudja <koudja@hotmail.fr>
2015-09-04 19:44:48 +0100

    Adding description to bending path

--- src/main/java/net/avatar/realms/spigot/bending/abilities/BendingPathType.java
+++ src/main/java/net/avatar/realms/spigot/bending/abilities/BendingPathType.java
@@ -3,25 +3,25 @@ package net.avatar.realms.spigot.bending.abilities;
 public enum BendingPathType {
 	None 		(BendingType.None),
 	
-	Spiritual	(BendingType.Air),
-	Mobile		(BendingType.Air),
-	Renegade	(BendingType.Air),
+	Spiritual	(BendingType.Air), // Default
+	Mobile		(BendingType.Air), // Add mobility - Reduce speed
+	Renegade	(BendingType.Air), // Add damage - Reduce range
 	
-	Balanced	(BendingType.Water),
-	Marksman	(BendingType.Water),
-	Flowless	(BendingType.Water),
+	Balanced	(BendingType.Water), // Default
+	Marksman	(BendingType.Water), // Add range - Reduce damage
+	Flowless	(BendingType.Water), // Add damage on ice - Remove redirection & control in-flight
 	
-	Control		(BendingType.Fire),
-	Nurture		(BendingType.Fire),
-	Lifeless	(BendingType.Fire),
+	Control		(BendingType.Fire), // Default
+	Nurture		(BendingType.Fire), // Add new DoT system - Reduce direct damage
+	Lifeless	(BendingType.Fire), // Add direct damage - Remove entirely DoT
 	
-	Patient		(BendingType.Earth),
-	Tough		(BendingType.Earth),
-	Reckless	(BendingType.Earth),
+	Patient		(BendingType.Earth), // Default
+	Tough		(BendingType.Earth), // Add resistance - Reduce damage
+	Reckless	(BendingType.Earth), // Add damage - Reduce speed
 	
-	Equality	(BendingType.ChiBlocker),
-	Seeker		(BendingType.ChiBlocker),
-	Restless	(BendingType.ChiBlocker);
+	Equality	(BendingType.ChiBlocker), // Default
+	Seeker		(BendingType.ChiBlocker), // Reduce cooldown - Reduce damage
+	Restless	(BendingType.ChiBlocker); // Add resistance - Augment cooldown
 	
 	
 
