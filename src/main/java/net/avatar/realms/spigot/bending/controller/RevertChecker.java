8556bd08b68d7d7057c169df69ec89ece2323e7d
Noko <nokorbis@gmail.com>
2015-09-04 23:06:22 +0200

    Direct access on Settings in RevertChecker

--- src/main/java/net/avatar/realms/spigot/bending/controller/RevertChecker.java
+++ src/main/java/net/avatar/realms/spigot/bending/controller/RevertChecker.java
@@ -7,95 +7,96 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 import java.util.logging.Level;
 
-import net.avatar.realms.spigot.bending.Bending;
-import net.avatar.realms.spigot.bending.abilities.Information;
-import net.avatar.realms.spigot.bending.utils.BlockTools;
-
 import org.bukkit.Chunk;
 import org.bukkit.Server;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
-public class RevertChecker implements Runnable {
+import net.avatar.realms.spigot.bending.Bending;
+import net.avatar.realms.spigot.bending.abilities.Information;
+import net.avatar.realms.spigot.bending.utils.BlockTools;
 
+public class RevertChecker implements Runnable {
+	
 	static Map<Block, Block> revertQueue = new HashMap<Block, Block>();
 	static Map<Integer, Integer> airRevertQueue = new HashMap<Integer, Integer>();
 	private Future<ArrayList<Chunk>> returnFuture;
-
+	
 	static Map<Chunk, Chunk> chunks = new HashMap<Chunk, Chunk>();
-
+	
 	private Bending plugin;
-
-	private static final boolean safeRevert = Settings.SAFE_REVERSE;
-
+	
 	private long time;
-
+	
 	public RevertChecker(Bending bending) {
-		plugin = bending;
+		this.plugin = bending;
 	}
-
+	
 	private class getOccupiedChunks implements Callable<ArrayList<Chunk>> {
-
+		
 		private Server server;
-
+		
 		public getOccupiedChunks(Server server) {
 			this.server = server;
 		}
-
+		
 		@Override
 		public ArrayList<Chunk> call() throws Exception {
 			ArrayList<Chunk> chunks = new ArrayList<Chunk>();
-
-			for (Player player : server.getOnlinePlayers()) {
+			
+			for (Player player : this.server.getOnlinePlayers()) {
 				Chunk chunk = player.getLocation().getChunk();
-				if (!chunks.contains(chunk))
+				if (!chunks.contains(chunk)) {
 					chunks.add(chunk);
+				}
 			}
 			return chunks;
 		}
 	}
-
+	
+	@Override
 	public void run() {
-		time = System.currentTimeMillis();
-
+		this.time = System.currentTimeMillis();
+		
 		if (Settings.REVERSE_BENDING) {
 			try {
-				returnFuture = plugin
+				this.returnFuture = this.plugin
 						.getServer()
 						.getScheduler()
-						.callSyncMethod(plugin,
-								new getOccupiedChunks(plugin.getServer()));
-				ArrayList<Chunk> chunks = returnFuture.get();
-
+						.callSyncMethod(this.plugin,
+								new getOccupiedChunks(this.plugin.getServer()));
+				ArrayList<Chunk> chunks = this.returnFuture.get();
+				
 				Map<Block, Information> earth = new HashMap<Block, Information>();
 				earth.putAll(BlockTools.bendedBlocks);
-
+				
 				for (Block block : earth.keySet()) {
 					if (revertQueue.containsKey(block)){
 						continue;
-					}				
+					}
 					boolean remove = true;
 					Information info = earth.get(block);
-					if (time < info.getTime() + Settings.REVERSE_TIME
-							|| (chunks.contains(block.getChunk()) && safeRevert)) {
+					if ((this.time < (info.getTime() + Settings.REVERSE_TIME))
+							|| (chunks.contains(block.getChunk()) && Settings.SAFE_REVERSE)) {
 						remove = false;
 					}
 					if (remove) {
 						addToRevertQueue(block);
 					}
 				}
-
+				
 				Map<Integer, Information> air = new HashMap<Integer, Information>();
 				air.putAll(BlockTools.tempAir);
-
+				
 				for (Integer i : air.keySet()) {
-					if (airRevertQueue.containsKey(i))
+					if (airRevertQueue.containsKey(i)) {
 						continue;
+					}
 					boolean remove = true;
 					Information info = air.get(i);
 					Block block = info.getBlock();
-					if (time < info.getTime() + Settings.REVERSE_TIME
-							|| (chunks.contains(block.getChunk()) && safeRevert)) {
+					if ((this.time < (info.getTime() + Settings.REVERSE_TIME))
+							|| (chunks.contains(block.getChunk()) && Settings.SAFE_REVERSE)) {
 						remove = false;
 					}
 					if (remove) {
@@ -103,20 +104,22 @@ public class RevertChecker implements Runnable {
 					}
 				}
 			} catch (Exception e) {
-				plugin.getLogger().log(Level.SEVERE, "Exception in revert checker", e);
+				this.plugin.getLogger().log(Level.SEVERE, "Exception in revert checker", e);
 			}
 		}
 	}
-
+	
 	private void addToAirRevertQueue(int i) {
-		if (!airRevertQueue.containsKey(i))
+		if (!airRevertQueue.containsKey(i)) {
 			airRevertQueue.put(i, i);
-
+		}
+		
 	}
-
+	
 	void addToRevertQueue(Block block) {
-		if (!revertQueue.containsKey(block))
+		if (!revertQueue.containsKey(block)) {
 			revertQueue.put(block, block);
+		}
 	}
-
+	
 }
