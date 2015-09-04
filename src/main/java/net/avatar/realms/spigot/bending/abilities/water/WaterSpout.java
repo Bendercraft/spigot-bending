4f1a2a3be7f96e265a0b8c22fad103d268c7e38b
Noko <nokorbis@gmail.com>
2015-09-04 21:53:13 +0200

    Correct WaterSpout, will change it later

--- src/main/java/net/avatar/realms/spigot/bending/abilities/water/WaterSpout.java
+++ src/main/java/net/avatar/realms/spigot/bending/abilities/water/WaterSpout.java
@@ -21,7 +21,7 @@ import net.avatar.realms.spigot.bending.abilities.TempBlock;
 import net.avatar.realms.spigot.bending.abilities.base.ActiveAbility;
 import net.avatar.realms.spigot.bending.abilities.base.IAbility;
 import net.avatar.realms.spigot.bending.controller.ConfigurationParameter;
-import net.avatar.realms.spigot.bending.controller.Flight;
+import net.avatar.realms.spigot.bending.controller.FlyingPlayer;
 import net.avatar.realms.spigot.bending.controller.Settings;
 import net.avatar.realms.spigot.bending.utils.BlockTools;
 import net.avatar.realms.spigot.bending.utils.EntityTools;
@@ -31,53 +31,52 @@ import net.avatar.realms.spigot.bending.utils.Tools;
 
 @BendingAbility(name="Water Spout", element=BendingType.Water)
 public class WaterSpout extends ActiveAbility {
-	
+
 	@ConfigurationParameter ("Rotation-Speed")
 	public static int SPEED = 4;
-	
+
 	@ConfigurationParameter ("Height")
 	private static int HEIGHT = 18;
-	
+
 	@ConfigurationParameter ("Cooldown")
 	private static long COOLDOWN = 0;
-	
+
 	//	private static final byte full = 0x0;
 	private int currentCardinalPoint = 0;
 	private Block base;
 	private BlockState baseState;
 	private Map<Block, BlockState> blocks;
+	private FlyingPlayer flying;
 
-	private boolean wasFlying = false;
-	private boolean wasAllowedToFly = false;
-	
 	public WaterSpout (Player player) {
 		super(player, null);
 	}
-	
+
 	@Override
 	public boolean swing () {
 		switch (this.state) {
 			case None:
 			case CannotStart:
 				return false;
-				
+
 			case CanStart:
 				if (canWaterSpout(this.player)) {
 					this.blocks = new HashMap<Block, BlockState>();
-					new Flight(this.player);
-					this.player.setAllowFlight(true);
-					spout();
-					setState(AbilityState.Progressing);
-					AbilityManager.getManager().addInstance(this);
+					this.flying = FlyingPlayer.addFlyingPlayer(this.player, Abilities.WaterSpout);
+					if (this.flying != null) {
+						spout();
+						setState(AbilityState.Progressing);
+						AbilityManager.getManager().addInstance(this);
+					}
 				}
 				return false;
-				
+
 			case Preparing:
 			case Prepared:
 			case Progressing:
 				setState(AbilityState.Ended);
 				return false;
-
+				
 			case Ending:
 			case Ended:
 			case Removed:
@@ -85,7 +84,7 @@ public class WaterSpout extends ActiveAbility {
 				return false;
 		}
 	}
-
+	
 	@Override
 	public boolean progress () {
 		if (!super.progress()) {
@@ -94,13 +93,13 @@ public class WaterSpout extends ActiveAbility {
 		revertSpout();
 		return spout();
 	}
-
+	
 	@Override
 	public void stop () {
 		revertSpout();
-		Flight.revert(this.player);
+		FlyingPlayer.removeFlyingPlayer(this.player, Abilities.WaterSpout);
 	}
-	
+
 	private void revertSpout () {
 		revertBaseBlock();
 		for (Block b : this.blocks.keySet()) {
@@ -108,32 +107,32 @@ public class WaterSpout extends ActiveAbility {
 		}
 		this.blocks.clear();
 	}
-	
+
 	@Override
 	public void remove () {
 		this.bender.cooldown(Abilities.WaterSpout, COOLDOWN);
 		super.remove();
 	}
-	
+
 	private boolean spout () {
 		this.player.setFallDistance(0);
 		this.player.setSprinting(false);
 		this.player.removePotionEffect(PotionEffectType.SPEED);
-		
+
 		Location location = this.player.getLocation().clone().add(0, 0.2, 0);
 		Block block = location.clone().getBlock();
 		int height = spoutableWaterHeight(location);
-		
+
 		if (height == -1) {
 			return false;
 		}
-		
+
 		location = this.base.getLocation();
 		for (int i = 1, cardinalPoint = this.currentCardinalPoint / SPEED; i <= height; i++, cardinalPoint++) {
 			if (cardinalPoint == 8) {
 				cardinalPoint = 0;
 			}
-			
+
 			block = location.clone().add(0, i, 0).getBlock();
 			if (!BlockTools.isTempBlock(block)) {
 				if (!isWaterSpoutBlock(block)) {
@@ -141,7 +140,7 @@ public class WaterSpout extends ActiveAbility {
 				}
 				block.setType(Material.WATER);
 			}
-			
+
 			switch (cardinalPoint) {
 				case 0:
 					block = location.clone().add(0, i, -1).getBlock();
@@ -170,7 +169,7 @@ public class WaterSpout extends ActiveAbility {
 				default:
 					break;
 			}
-			
+
 			if (block.getType().equals(Material.AIR) || isWaterSpoutBlock(block)) {
 				if (!BlockTools.isTempBlock(block)) {
 					if (!isWaterSpoutBlock(block)) {
@@ -184,19 +183,17 @@ public class WaterSpout extends ActiveAbility {
 		if (this.currentCardinalPoint == (SPEED * 8)) {
 			this.currentCardinalPoint = 0;
 		}
-		
+
 		if (this.player.getLocation().getBlockY() > block.getY()) {
-			Flight.revert(this.player);
+			this.flying.resetState();
 		} else {
-			new Flight(this.player);
-			this.player.setAllowFlight(true);
-			this.player.setFlying(true);
+			this.flying.fly();
 		}
 		return true;
 	}
-	
+
 	private int spoutableWaterHeight (Location location) {
-		
+
 		int height = HEIGHT;
 		if (Tools.isNight(this.player.getWorld())) {
 			height = (int) PluginTools.waterbendingNightAugment(
@@ -246,20 +243,20 @@ public class WaterSpout extends ActiveAbility {
 		revertBaseBlock();
 		return -1;
 	}
-
+	
 	private void revertBaseBlock () {
 		if (this.baseState != null) {
 			this.baseState.update(true);
 			this.baseState = null;
 		}
 	}
-
+	
 	public static boolean isWaterSpoutBlock (Block block) {
 		Map<Object, IAbility> instances = AbilityManager.getManager().getInstances(Abilities.WaterSpout);
 		if ((instances == null) || instances.isEmpty()) {
 			return false;
 		}
-
+		
 		for (IAbility ab : instances.values()) {
 			WaterSpout spout = (WaterSpout) ab;
 			if (spout.blocks.containsKey(block)) {
@@ -268,7 +265,7 @@ public class WaterSpout extends ActiveAbility {
 		}
 		return false;
 	}
-	
+
 	public static List<Player> getPlayers() {
 		Map<Object, IAbility> instances = AbilityManager.getManager().getInstances(Abilities.WaterSpout);
 		LinkedList<Player> players = new LinkedList<Player>();
@@ -280,13 +277,13 @@ public class WaterSpout extends ActiveAbility {
 		}
 		return players;
 	}
-	
+
 	public static void removeSpouts (Location loc0, double radius, Player sourceplayer) {
 		Map<Object, IAbility> instances = AbilityManager.getManager().getInstances(Abilities.WaterSpout);
 		if (instances == null) {
 			return;
 		}
-
+		
 		for (Object obj : instances.keySet()) {
 			Player player = (Player) obj;
 			if (!player.equals(sourceplayer)) {
@@ -295,16 +292,16 @@ public class WaterSpout extends ActiveAbility {
 				double dx = loc1.getX() - loc0.getX();
 				double dy = loc1.getY() - loc0.getY();
 				double dz = loc1.getZ() - loc0.getZ();
-				
+
 				double distance = Math.sqrt((dx * dx) + (dz * dz));
-				
+
 				if ((distance <= radius) && (dy > 0) && (dy < HEIGHT)) {
 					instances.get(player).consume();
 				}
 			}
 		}
 	}
-	
+
 	public static boolean isBending(Player player) {
 		Map<Object, IAbility> instances = AbilityManager.getManager().getInstances(Abilities.WaterSpout);
 		if (instances == null) {
@@ -312,7 +309,7 @@ public class WaterSpout extends ActiveAbility {
 		}
 		return instances.containsKey(player);
 	}
-	
+
 	public static boolean canWaterSpout(Player player) {
 		Location loc = player.getLocation();
 		if (BlockTools.isWaterBased(loc.getBlock())){
@@ -328,12 +325,12 @@ public class WaterSpout extends ActiveAbility {
 		}
 		return false;
 	}
-	
+
 	@Override
 	public Object getIdentifier () {
 		return this.player;
 	}
-	
+
 	@Override
 	public Abilities getAbilityType () {
 		return Abilities.WaterSpout;
--- src/main/java/net/avatar/realms/spigot/bending/controller/FlyingPlayer.java
+++ src/main/java/net/avatar/realms/spigot/bending/controller/FlyingPlayer.java
@@ -15,79 +15,75 @@ import net.avatar.realms.spigot.bending.abilities.Abilities;
 // If your were WaterSpouting when a tornado comes against you, there may be a conflict giving the
 // flying to a player
 public class FlyingPlayer {
-	
-	private static Map<UUID, FlyingPlayer> flyingPlayers = new HashMap<UUID, FlyingPlayer>();
 
+	private static Map<UUID, FlyingPlayer> flyingPlayers = new HashMap<UUID, FlyingPlayer>();
+	
 	private Player player;
 	private boolean couldFly;
 	private boolean wasFlying;
 	private List<Abilities> causes;
-	
+
 	private FlyingPlayer (Player player) {
 		this.player = player;
 		this.couldFly = player.getAllowFlight();
 		this.wasFlying = player.isFlying();
 		this.causes = new LinkedList<Abilities>();
 	}
-	
-	private void fly () {
+
+	public void fly () {
 		this.player.setAllowFlight(true);
 		this.player.setFlying(true);
 	}
-	
-	private void resetState () {
+
+	public void resetState () {
 		this.player.setAllowFlight(this.couldFly);
 		this.player.setFlying(this.wasFlying);
 	}
-	
+
 	public boolean addCause (Abilities cause) {
 		if (this.causes == null) {
 			return false;
 		}
-
+		
 		this.causes.add(cause);
 		return true;
 	}
-	
+
 	public boolean hasCauses () {
 		if (this.causes == null) {
 			return false;
 		}
-		
-		if (this.causes.isEmpty()) {
-			return false;
-		}
-		
-		return true;
-	}
 
+		return !this.causes.isEmpty();
+	}
+	
 	public boolean hasCause (Abilities cause) {
 		if (this.causes == null) {
 			return false;
 		}
-		
+
 		if (this.causes.isEmpty()) {
 			return false;
 		}
-
+		
 		return this.causes.contains(cause);
 	}
-
+	
 	public void removeCause (Abilities cause) {
 		if (this.causes == null) {
 			return;
 		}
-
+		
 		if (this.causes.contains(cause)) {
 			this.causes.remove(cause);
 		}
 	}
-	
-	public static void addFlyingPlayer (Player player, Abilities cause) {
-		if ((player == null) || (cause == null)) {
-			return;
-		}
 
+	public static FlyingPlayer addFlyingPlayer (Player player, Abilities cause) {
+		if ((player == null) || (cause == null)) {
+			return null;
+		}
+		
 		FlyingPlayer flying = null;
 		if (flyingPlayers.containsKey(player.getUniqueId())) {
 			flying = flyingPlayers.get(player.getUniqueId());
@@ -98,25 +94,26 @@ public class FlyingPlayer {
 		else {
 			flying = new FlyingPlayer(player);
 		}
-
+		
 		flying.addCause(cause);
 		if (flying.hasCauses()) {
 			flyingPlayers.put(player.getUniqueId(), flying);
 			flying.fly();
 		}
+		return flying;
 	}
-	
+
 	public static void removeFlyingPlayer (Player player, Abilities cause) {
 		if (!flyingPlayers.containsKey(player.getUniqueId())) {
 			return;
 		}
-
+		
 		FlyingPlayer flying = flyingPlayers.get(player.getUniqueId());
 		if (flying == null) {
 			flyingPlayers.remove(player.getUniqueId());
 			return;
 		}
-
+		
 		if (flying.hasCause(cause)) {
 			flying.removeCause(cause);
 			if (!flying.hasCauses()) {
@@ -125,5 +122,4 @@ public class FlyingPlayer {
 			}
 		}
 	}
-
 }
