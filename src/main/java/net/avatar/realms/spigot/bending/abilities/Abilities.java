bcd113d24bf8c84f0abb7d2946ed1a06c53d0aef
Noko <nokorbis@gmail.com>
2015-09-20 13:44:45 +0200

    Simplify some abilities stuff

--- src/main/java/net/avatar/realms/spigot/bending/abilities/Abilities.java
+++ src/main/java/net/avatar/realms/spigot/bending/abilities/Abilities.java
@@ -7,7 +7,7 @@ import java.util.List;
  * This class list all abilities
  */
 public enum Abilities {
-
+	
 	/*
 	 * AIR Abilities
 	 */
@@ -23,11 +23,11 @@ public enum Abilities {
 	AirManipulation (BendingType.Air, true),
 	Tornado (BendingSpecializationType.Tornado, true),
 	Suffocate (BendingSpecializationType.Suffocate, true),
-
+	
 	/*
 	 * EARTH Abilities
 	 */
-
+	
 	Catapult (BendingType.Earth, true),
 	RaiseEarth (BendingType.Earth, true),
 	EarthGrab (BendingType.Earth, true),
@@ -39,11 +39,11 @@ public enum Abilities {
 	Shockwave (BendingType.Earth, true),
 	LavaTrain (BendingSpecializationType.Lavabend, true),
 	MetalBending (BendingSpecializationType.Metalbend, true),
-
+	
 	/*
 	 * FIRE Abilities
 	 */
-
+	
 	HeatControl (BendingType.Fire, true),
 	Blaze (BendingType.Fire, true),
 	FireJet (BendingType.Fire, false),
@@ -55,11 +55,11 @@ public enum Abilities {
 	FireBlade (BendingType.Fire, false),
 	Combustion (BendingSpecializationType.Combustion, true),
 	Lightning (BendingSpecializationType.Lightning, true),
-
+	
 	/*
 	 * WATER Abilities
 	 */
-
+	
 	WaterBubble (BendingType.Water, true),
 	PhaseChange (BendingType.Water, true),
 	HealingWaters (BendingType.Water, true),
@@ -72,11 +72,11 @@ public enum Abilities {
 	IceSwipe (BendingType.Water, true),
 	Bloodbending (BendingSpecializationType.Bloodbend, true),
 	Drainbending (BendingSpecializationType.DrainBend, false),
-
+	
 	/*
 	 * CHI-BLOCKERS Abilities
 	 */
-
+	
 	HighJump (BendingType.ChiBlocker, false),
 	ChiSpeed (BendingType.ChiBlocker, false),
 	RapidPunch (BendingType.ChiBlocker, false),
@@ -86,65 +86,65 @@ public enum Abilities {
 	PowerfulHit (BendingType.ChiBlocker, false),
 	PoisonnedDart (BendingSpecializationType.Inventor, false),
 	PlasticBomb (BendingSpecializationType.Inventor, true),
-
+	
 	/*
 	 * AVATAR Abilities
 	 */
-
+	
 	AvatarState (BendingType.Energy, false),
-
+	
 	/*
 	 * PASSIVE Abilities
 	 */
-
+	
 	AirPassive (BendingType.Air, false),
 	ChiPassive (BendingType.ChiBlocker, false),
 	EarthPassive (BendingType.Earth, false),
 	FirePassive (BendingType.Fire, false),
 	FastSwimming (BendingType.Water, true),
 	WaterPassive (BendingType.Water, false),
-
+	
 	/*
 	 * UTILITY Abilities
 	 */
-
+	
 	FireStream (BendingType.Fire, false);
-
+	
 	/*
 	 *
 	 */
-
+	
 	private BendingType element;
 	private BendingSpecializationType specialization;
 	private boolean shift;
-
+	
 	Abilities (BendingType element, boolean shift) {
 		this.element = element;
 		this.specialization = null;
 		this.shift = shift;
 	}
-
+	
 	Abilities (BendingSpecializationType specialization, boolean shift) {
 		this.element = specialization.getElement();
 		this.specialization = specialization;
 		this.shift = shift;
 	}
-
+	
 	public BendingType getElement () {
 		return this.element;
 	}
-
+	
 	public BendingSpecializationType getSpecialization () {
 		return this.specialization;
 	}
-
+	
 	public boolean isSpecialization () {
 		if (this.specialization != null) {
 			return true;
 		}
 		return false;
 	}
-
+	
 	// Static methods
 	public static Abilities getAbility (String ability) {
 		for (Abilities a : Abilities.values()) {
@@ -154,115 +154,115 @@ public enum Abilities {
 		}
 		return null;
 	}
-
+	
 	public static boolean isAirbending (Abilities ability) {
 		return ability.isAirbending();
 	}
-
+	
 	public boolean isAirbending () {
 		return getElement().equals(BendingType.Air);
 	}
-
+	
 	public static List<Abilities> getAirbendingAbilities () {
 		List<Abilities> result = new LinkedList<Abilities>();
 		for (Abilities a : Abilities.values()) {
-			if (isAirbending(a)) {
+			if (a.isAirbending()) {
 				result.add(a);
 			}
 		}
 		return result;
 	}
-
+	
 	public static boolean isWaterbending (Abilities ability) {
 		return ability.isWaterbending();
 	}
-
+	
 	public boolean isWaterbending () {
 		return getElement().equals(BendingType.Water);
 	}
-
+	
 	public static List<Abilities> getWaterbendingAbilities () {
 		List<Abilities> result = new LinkedList<Abilities>();
 		for (Abilities a : Abilities.values()) {
-			if (isWaterbending(a)) {
+			if (a.isWaterbending()) {
 				result.add(a);
 			}
 		}
 		return result;
 	}
-
+	
 	public static boolean isEarthbending (Abilities ability) {
 		return ability.isEarthbending();
 	}
-
+	
 	public boolean isEarthbending () {
 		return getElement().equals(BendingType.Earth);
 	}
-
+	
 	public static List<Abilities> getEarthbendingAbilities () {
 		List<Abilities> result = new LinkedList<Abilities>();
 		for (Abilities a : Abilities.values()) {
-			if (isEarthbending(a)) {
+			if (a.isEarthbending()) {
 				result.add(a);
 			}
 		}
 		return result;
 	}
-
+	
 	public static boolean isFirebending (Abilities ability) {
 		return ability.isFirebending();
 	}
-
+	
 	public boolean isFirebending () {
 		return getElement().equals(BendingType.Fire);
 	}
-
+	
 	public static List<Abilities> getFirebendingAbilities () {
 		List<Abilities> result = new LinkedList<Abilities>();
 		for (Abilities a : Abilities.values()) {
-			if (isFirebending(a)) {
+			if (a.isFirebending()) {
 				result.add(a);
 			}
 		}
 		return result;
 	}
-
+	
 	public static boolean isChiBlocking (Abilities ability) {
 		return ability.isChiblocking();
 	}
-
+	
 	public boolean isChiblocking () {
 		return getElement().equals(BendingType.ChiBlocker);
 	}
-
+	
 	public static List<Abilities> getChiBlockingAbilities () {
 		List<Abilities> result = new LinkedList<Abilities>();
 		for (Abilities a : Abilities.values()) {
-			if (isChiBlocking(a)) {
+			if (a.isChiblocking()) {
 				result.add(a);
 			}
 		}
 		return result;
 	}
-
+	
 	public boolean isShiftAbility () {
 		return this.shift;
 	}
-
+	
 	public static boolean isEnergyAbility (Abilities ab) {
 		if (ab.getElement().equals(BendingType.Energy)) {
 			return true;
 		}
 		return false;
 	}
-
+	
 	public boolean isEnergyAbility () {
 		if (getElement().equals(BendingType.Energy)) {
 			return true;
 		}
 		return false;
 	}
-
+	
 	// TODO : Make sure passive abilities cannot be bound
 	public boolean isPassiveAbility () {
 		switch (this) {
@@ -277,10 +277,10 @@ public enum Abilities {
 				return true;
 			default:
 				return false;
-
+				
 		}
 	}
-
+	
 	public boolean isUtilityAbility () {
 		switch (this) {
 			case FireStream:
@@ -289,7 +289,7 @@ public enum Abilities {
 				return false;
 		}
 	}
-	
+
 	public String getPermission () {
 		return "bending." + getElement().toString().toLowerCase() + name().toLowerCase();
 	}
--- src/main/java/net/avatar/realms/spigot/bending/utils/ProtectionManager.java
+++ src/main/java/net/avatar/realms/spigot/bending/utils/ProtectionManager.java
@@ -26,23 +26,23 @@ import net.citizensnpcs.api.trait.Trait;
 
 
 public class ProtectionManager {
-
+	
 	private static Set<Abilities> allowedEverywhereAbilities = new HashSet<Abilities>();
 	static {
 		allowedEverywhereAbilities.add(Abilities.HealingWaters);
 	}
-
+	
 	private static PluginManager pm;
-
+	
 	private static WorldGuardPlugin worldguard = null;
 	private static WGCustomFlagsPlugin wgCustomFlags = null;
-	
+
 	private static boolean useWG = false;
 	private static boolean useCustomFlagsWG = false;
-	private static boolean useFactions = false;
+	//	private static boolean useFactions = false;
 	private static boolean useCitizens = false;
-	
 
+	
 	private static StateFlag BENDING;
 	private static StateFlag BENDING_AIR;
 	private static StateFlag BENDING_CHI;
@@ -52,7 +52,7 @@ public class ProtectionManager {
 	private static StateFlag BENDING_PASSIVES;
 	private static StateFlag BENDING_SPE;
 	private static StateFlag BENDING_ENERGY;
-
+	
 	public static void init () {
 		// WorldGuard
 		if (Settings.RESPECT_WORLDGUARD) {
@@ -62,14 +62,14 @@ public class ProtectionManager {
 				worldguard = (WorldGuardPlugin) plugin;
 				useWG = true;
 			}
-
+			
 			plugin = pm.getPlugin("WGCustomFlags");
 			if ((plugin != null) && (plugin.isEnabled())) {
 				wgCustomFlags = (WGCustomFlagsPlugin) plugin;
 				useCustomFlagsWG = true;
 			}
 		}
-
+		
 		if (useWG && useCustomFlagsWG) {
 			BENDING = 			new StateFlag("bending", true);
 			BENDING_AIR = 		new StateFlag("bending-air", true);
@@ -80,8 +80,8 @@ public class ProtectionManager {
 			BENDING_PASSIVES = 	new StateFlag("bending-passives", true);
 			BENDING_SPE = 		new StateFlag("bending-spe", true);
 			BENDING_ENERGY = 	new StateFlag("bending-energy", true);
-
-
+			
+			
 			wgCustomFlags.addCustomFlag(BENDING);
 			wgCustomFlags.addCustomFlag(BENDING_PASSIVES);
 			wgCustomFlags.addCustomFlag(BENDING_AIR);
@@ -92,21 +92,19 @@ public class ProtectionManager {
 			wgCustomFlags.addCustomFlag(BENDING_SPE);
 			wgCustomFlags.addCustomFlag(BENDING_ENERGY);
 		}
-
-
-
-		Plugin fcp = Bukkit.getPluginManager().getPlugin("Factions");
-		if (fcp != null) {
-			PluginTools.verbose("Recognized Factions...");
-			if (Settings.RESPECT_FACTIONS) {
-				useFactions = true;
-				PluginTools.verbose("Bending is set to respect Factions' claimed lands.");
-			}
-			else {
-				PluginTools.verbose("But Bending is set to ignore Factions' claimed lands.");
-			}
-		}
-
+		
+		//		Plugin fcp = Bukkit.getPluginManager().getPlugin("Factions");
+		//		if (fcp != null) {
+		//			PluginTools.verbose("Recognized Factions...");
+		//			if (Settings.RESPECT_FACTIONS) {
+		//				useFactions = true;
+		//				PluginTools.verbose("Bending is set to respect Factions' claimed lands.");
+		//			}
+		//			else {
+		//				PluginTools.verbose("But Bending is set to ignore Factions' claimed lands.");
+		//			}
+		//		}
+		
 		Plugin citizens = Bukkit.getPluginManager().getPlugin("Citizens");
 		if (citizens != null) {
 			PluginTools.verbose("Recognized Citizens...");
@@ -119,7 +117,7 @@ public class ProtectionManager {
 			}
 		}
 	}
-
+	
 	public static boolean isEntityProtectedByCitizens (Entity entity) {
 		if (useCitizens) {
 			if (CitizensAPI.getNPCRegistry().isNPC(entity)) {
@@ -132,12 +130,12 @@ public class ProtectionManager {
 		}
 		return false;
 	}
-
+	
 	public static boolean isRegionProtectedFromExplosion (Player player, Abilities ability, Location loc) {
 		if (isRegionProtectedFromBending(player, ability, loc)) {
 			return true;
 		}
-
+		
 		if (useWG) {
 			LocalPlayer localPlayer = worldguard.wrapPlayer(player);
 			for (Location location : new Location[] {loc, player.getLocation()}) {
@@ -153,76 +151,76 @@ public class ProtectionManager {
 		}
 		return false;
 	}
-
+	
 	public static boolean isRegionProtectedFromBending (Player player, Abilities ability, Location loc) {
 		if (!useWG) {
 			return false;
 		}
-
+		
 		if (ability == null) {
 			return false;
 		}
-
+		
 		if (player.isOp()) {
 			return false;
 		}
-
+		
 		if (isAllowedEverywhereAbility(ability)) {
 			return false;
 		}
 		LocalPlayer localPlayer = worldguard.wrapPlayer(player);
 		RegionContainer container = worldguard.getRegionContainer();
 		RegionQuery query = container.createQuery();
-		
+
 		if (ability.isPassiveAbility()) {
 			if (!query.testState(loc, localPlayer, BENDING_PASSIVES)) {
 				return true;
 			}
 			return false;
 		}
-
+		
 		if (!query.testState(loc, localPlayer, BENDING)) {
 			return true;
 		}
-
+		
 		if (ability.isSpecialization()
 				&& !query.testState(loc, localPlayer, BENDING_SPE)) {
 			return true;
 		}
-
+		
 		if ((ability == Abilities.AvatarState)
 				&& !query.testState(loc, localPlayer, BENDING_ENERGY)) {
 			return true;
 		}
-
-		if (Abilities.isAirbending(ability)
+		
+		if (ability.isAirbending()
 				&& !query.testState(loc, localPlayer, BENDING_AIR)) {
 			return true;
 		}
-
-		if (Abilities.isChiBlocking(ability)
+		
+		if (ability.isChiblocking()
 				&& !query.testState(loc, localPlayer, BENDING_CHI)) {
 			return true;
 		}
-
-		if (Abilities.isEarthbending(ability)
+		
+		if (ability.isEarthbending()
 				&& !query.testState(loc, localPlayer, BENDING_EARTH)) {
 			return true;
 		}
-
-		if (Abilities.isFirebending(ability)
+		
+		if (ability.isFirebending()
 				&& !query.testState(loc, localPlayer, BENDING_FIRE)) {
 			return true;
 		}
-
-		if (Abilities.isWaterbending(ability)
+		
+		if (ability.isWaterbending()
 				&& !query.testState(loc, localPlayer, BENDING_WATER)) {
 			return true;
 		}
-
+		
 		return false;
 	}
-
+	
 	public static boolean isRegionProtectedFromBendingPassives(Player player, Location loc) {
 		if (useWG) {
 			LocalPlayer localPlayer = worldguard.wrapPlayer(player);
@@ -232,7 +230,7 @@ public class ProtectionManager {
 		}
 		return false;
 	}
-
+	
 	public static boolean isAllowedEverywhereAbility (Abilities ability) {
 		return allowedEverywhereAbilities.contains(ability);
 	}
