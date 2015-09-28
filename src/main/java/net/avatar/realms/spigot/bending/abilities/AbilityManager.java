2674da02ec307a4af62e57b81a7737ac84847409
Kiratsu <deboisjerome@gmail.com>
2015-09-28 07:57:46 +0200

    FastSwimming should be working now

--- src/main/java/net/avatar/realms/spigot/bending/abilities/AbilityManager.java
+++ src/main/java/net/avatar/realms/spigot/bending/abilities/AbilityManager.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
