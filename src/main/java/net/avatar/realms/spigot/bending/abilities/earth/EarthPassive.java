9ffa8038793222286f279f8860f733f5d191d45f
koudja <koudja@hotmail.fr>
2015-10-25 10:00:12 +0000

    EarthPassive should use TempBlock

--- src/main/java/net/avatar/realms/spigot/bending/abilities/earth/EarthPassive.java
+++ src/main/java/net/avatar/realms/spigot/bending/abilities/earth/EarthPassive.java
@@ -1,12 +1,12 @@
 package net.avatar.realms.spigot.bending.abilities.earth;
 
-import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 
 import net.avatar.realms.spigot.bending.abilities.BendingAbilities;
@@ -19,6 +19,7 @@ import net.avatar.realms.spigot.bending.abilities.BendingPassiveAbility;
 import net.avatar.realms.spigot.bending.controller.ConfigurationParameter;
 import net.avatar.realms.spigot.bending.utils.BlockTools;
 import net.avatar.realms.spigot.bending.utils.EntityTools;
+import net.avatar.realms.spigot.bending.utils.TempBlock;
 
 @ABendingAbility(name = "Earth Passive", bind = BendingAbilities.EarthPassive, element = BendingElement.Earth)
 public class EarthPassive extends BendingPassiveAbility {
@@ -26,7 +27,7 @@ public class EarthPassive extends BendingPassiveAbility {
 	@ConfigurationParameter("Time-Before-Reverse")
 	private static long DURATION = 2500;
 
-	private Map<Block, BlockState> blocks = new HashMap<Block, BlockState>();
+	private List<TempBlock> blocks = new LinkedList<TempBlock>();
 
 	public EarthPassive(Player player) {
 		super(player);
@@ -41,8 +42,7 @@ public class EarthPassive extends BendingPassiveAbility {
 			if (!BlockTools.isTransparentToEarthbending(player, block)) {
 				if (BlockTools.isSolid(block.getRelative(BlockFace.DOWN))) {
 					if (!isPassiveSand(block)) {
-						blocks.put(block, block.getState());
-						block.setType(Material.SAND);
+						blocks.add(new TempBlock(block, Material.SAND, (byte) 0x0));
 					}
 				}
 			}
@@ -51,8 +51,7 @@ public class EarthPassive extends BendingPassiveAbility {
 				if (BlockTools.isEarthbendable(player, affectedblock) && !BlockTools.isIronBendable(player, affectedblock.getType())) {
 					if (BlockTools.isSolid(affectedblock.getRelative(BlockFace.DOWN))) {
 						if (!isPassiveSand(affectedblock) && !block.getLocation().equals(affectedblock.getLocation())) {
-							blocks.put(affectedblock, affectedblock.getState());
-							affectedblock.setType(Material.SAND);
+							blocks.add(new TempBlock(affectedblock, Material.SAND, (byte) 0x0));
 						}
 					}
 				}
@@ -72,8 +71,8 @@ public class EarthPassive extends BendingPassiveAbility {
 
 	@Override
 	public void stop() {
-		for (BlockState state : blocks.values()) {
-			state.update(true);
+		for (TempBlock state : blocks) {
+			state.revertBlock();
 		}
 		blocks.clear();
 	}
@@ -83,8 +82,11 @@ public class EarthPassive extends BendingPassiveAbility {
 		if (instances == null || instances.isEmpty()) {
 			return false;
 		}
+		if(!TempBlock.isTempBlock(block)) {
+			return false;
+		}
 		for (BendingAbility passive : instances.values()) {
-			if (((EarthPassive) passive).blocks.containsKey(block)) {
+			if (((EarthPassive) passive).blocks.contains(TempBlock.get(block))) {
 				return true;
 			}
 		}
@@ -92,18 +94,9 @@ public class EarthPassive extends BendingPassiveAbility {
 	}
 
 	public static void revertSand(Block block) {
-		EarthPassive passive = null;
-		for (BendingAbility abil : AbilityManager.getManager().getInstances(BendingAbilities.EarthPassive).values()) {
-			if (((EarthPassive) abil).blocks.containsKey(block)) {
-				passive = ((EarthPassive) abil);
-				break;
-			}
+		if(isPassiveSand(block)) {
+			TempBlock.get(block).revertBlock();
 		}
-		if (passive == null) {
-			return;
-		}
-		passive.blocks.get(block).update(true);
-		passive.blocks.remove(block);
 	}
 
 	@Override
