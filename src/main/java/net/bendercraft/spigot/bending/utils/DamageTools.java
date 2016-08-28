0c21b1c0ea710d4b3de1de38af8ca1293484b3b1
koudja <koudja@hotmail.fr>
2016-08-29 00:04:48 +0200

    And put value as it is needed.

--- src/main/java/net/bendercraft/spigot/bending/utils/DamageTools.java
+++ src/main/java/net/bendercraft/spigot/bending/utils/DamageTools.java
@@ -71,8 +71,12 @@ public class DamageTools {
 	        Map<DamageModifier, Function<? super Double, Double>> modifierFunctions = new EnumMap<DamageModifier, Function<? super Double, Double>>(DamageModifier.class);
 	        
 	        modifiers.put(DamageModifier.BASE, damage);
+	        modifierFunctions.put(DamageModifier.BASE, ZERO);
 	        for(DamageModifier modifier : DamageModifier.values()) {
-	        	modifierFunctions.put(modifier, ZERO);
+	        	if(modifier != DamageModifier.BASE) {
+		        	modifiers.put(modifier, 0.0);
+		        	modifierFunctions.put(modifier, ZERO);
+	        	}
 	        }
 			
 			BendingDamageEvent event = new BendingDamageEvent(attacker, damagee, ability, modifiers, modifierFunctions);
